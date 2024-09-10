99bc547699dae887b3b34a874e685caf86cd42e7
ya <150337096+yasu-cr@users.noreply.github.com>
2024-09-10 20:06:04 +0900

    Implement basic login

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,29 @@
-require "test_helper"
+class UsersController < ApplicationController
 
-class UsersControllerTest < ActionDispatch::IntegrationTest
-
-  test "should get new" do
-    get signup_path
-    assert_response :success
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      reset_session
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
